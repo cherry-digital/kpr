6d1740bb54c8d4d091458b6d851d0a0daab18e6f
cherry-digital <98630255+cherry-digital@users.noreply.github.com>
2022-02-11 20:07:54 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="cs">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Kvalitní prezentace a eshopy</title>
+    <link href="css/reset.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Alegreya:400,400i,700,700i,900&subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700&subset=latin-ext" rel="stylesheet">
+</head>
+<body>
+  <div id="page">
+   <div class="container">
+     <p class="pripravujeme">Jmenuji se <strong>Ondřej Švajnoch</strong> a jsem zkušený<br />
+    <span class="restaurace">webový Developer</span><span class="wellness"></span> <br />
+    </p>
+     <p class="pripravujeme">&bdquo;Tvorba webů není jen moje práce, je to moje vášeň&ldquo;</p>
+    
+    <p class="termin">
+    Vytvořím Vám kvalitní web, který bude pro zákazníky opravdu vidět!<br />
+    </p>
+    <p class="termin">&nbsp;</p>
+    
+    <div class="clear">
+    <div class="half">
+      <h4>CO NABÍZÍM?</h4>
+      <p>&nbsp;</p>
+      <p>kvalitní responzivní weby<br />
+          eshopy, seo a programování</p>
+      <p>programování, ppc, reklamní strategie</p>
+    </div>
+    <div class="half">
+      <h4>KONTAKTY:</h4>
+      <p>&nbsp;</p>
+      <p><strong>Ondřej Švajnoch</strong> <br />
+      <a href="mailto:ondrej@kvalitniprezentace.cz">ondrej@kvalitniprezentace.cz</a> <br />
+      Praha - Brno - Plzeň</p>
+    </div>
+    <p>&nbsp;</p>
+    <p>Mám 10let zkušeností, moji zákazníci jsou velké  firmy, ale i drobnější podnikatelé. </p>
+    <p>&nbsp;</p>
+    <p>Přinesu výsledky Vašemu podnikání. Preferuji dlouhodobou spolupráci.    </p>
+    <p>&nbsp;</p>
+    <p>Pojďme se nezávazně sejít - překvapím Vás...</p>
+    <p>&nbsp;</p>
+    </div></div></div></body>
+</html>
